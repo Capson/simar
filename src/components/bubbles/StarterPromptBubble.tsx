4c6caf8de1dc68df6303082aa276bac71546b56f
simar525 <ssimarpreet11@gmail.com>
2024-05-25 23:39:58 +0530

    v11

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -8,32 +8,32 @@ export const StarterPromptBubble = (props: Props) => (
     <div
       data-modal-target="defaultModal"
       data-modal-toggle="defaultModal"
-      className="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
+      class="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
       onClick={() => props.onPromptClick?.()}
     >
       <span
-        className="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
+        class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
         data-testid="host-bubble"
         style={{
           display: 'inline-block',
-          borderRadius: '10px', // Adjust as needed
+          'border-radius': '10px', // Adjust as needed
           padding: '5px', // Space for the border
-          background: 'linear-gradient(45deg, violet, purple, #E6E6FA, green)', // Gradient colors
-          WebkitMask: 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
+          background: 'linear-gradient(45deg, violet, 00338D, #E6E6FA, 005EB8)', // Gradient colors
+          '-webkit-mask': 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
           mask: 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
-          WebkitMaskComposite: 'destination-out',
-          maskComposite: 'exclude',
+          '-webkit-mask-composite': 'destination-out',
+          'mask-composite': 'exclude',
         }}
       >
         <span
-          className="content"
+          class="content"
           style={{
             display: 'block',
-            borderRadius: '10px', // Match the outer border radius
-            padding: '20px', // Adjust as needed
+            'border-radius': '10px', // Match the outer border radius
+            padding: '2px', // Adjust as needed
             background: '#ffffff', // Background of the content area
             width: 'max-content',
-            fontSize: '15px',
+            'font-size': '15px',
             cursor: 'pointer',
           }}
         >
