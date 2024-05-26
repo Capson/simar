7616e1904b3db551a9b27d8cdbe5fdbed7fcaad7
simar525 <ssimarpreet11@gmail.com>
2024-05-26 17:57:40 +0530

    v29

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -4,36 +4,34 @@ type Props = {
 };
 
 export const StarterPromptBubble = (props: Props) => (
-  <>
+  <div
+    data-modal-target="defaultModal"
+    data-modal-toggle="defaultModal"
+    className="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
+    onClick={() => props.onPromptClick?.()}
+    style={{
+      position: 'relative',
+      display: 'inline-block',
+      borderRadius: '12px', 
+      padding: '2px',
+      background: 'linear-gradient(to right, violet, red)',
+    }}
+  >
     <div
-      data-modal-target="defaultModal"
-      data-modal-toggle="defaultModal"
-      className="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
-      onClick={() => props.onPromptClick?.()}
+      className="px-2 py-1 whitespace-pre-wrap max-w-full"
+      data-testid="host-bubble"
       style={{
-        display: 'inline-block',
-        padding: '4px', // Slight padding to show the border
-        borderRadius: '12px', // Same radius for outer div
-        background: 'linear-gradient(to right, violet, red)', // Gradient border
-      }}
-    >
-      {/* Inner content with solid background */}
-      <span
-        className="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
-        data-testid="host-bubble"
-        style={{
           display: 'inline-block',
           width: 'max-content',
           fontSize: '15px',
-          borderRadius: '10px', // Slightly smaller radius for inner span
+          borderRadius: '12px', 
           cursor: 'pointer',
           backgroundColor: 'white', // Background for inner content
           padding: '0.5em', // Inner padding for content
           color: 'black', // Text color
-        }}
-      >
-        {props.prompt}
-      </span>
+      }}
+    >
+      {props.prompt}
     </div>
-  </>
+  </div>
 );
