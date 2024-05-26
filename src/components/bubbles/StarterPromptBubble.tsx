7547fdedd0e2518be9c3d2ccbfb68f3e3e8df3ac
simar525 <ssimarpreet11@gmail.com>
2024-05-26 18:28:54 +0530

    v35

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -19,11 +19,11 @@ export const StarterPromptBubble = (props: Props) => (
           'font-size': '15px',
           'border-radius': '0.5em', // Combined both border-radius
           cursor: 'pointer',
-          // border: '1px solid purple', // Added border style
-          border: 'solid;  border-image: linear-gradient(to right, #00338D, #6D2077) 1 / 5px',
+          border: '2px solid #6D2077', // Added border style
           padding: '0.5em', // Added padding for space inside the border
-          // background: 'linear-gradient(to right, #00338D, #6D2077)',
-          // color: 'white',
+          background: 'linear-gradient(to right, #00338D, #6D2077)',
+          color: 'white',
+
         }}
       >
         {props.prompt}
