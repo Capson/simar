763b2e004e417e9a4452f415838d9bc92efd3dab
simar525 <ssimarpreet11@gmail.com>
2024-05-26 20:11:41 +0530

    v36

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -8,23 +8,25 @@ export const StarterPromptBubble = (props: Props) => (
     <div
       data-modal-target="defaultModal"
       data-modal-toggle="defaultModal"
-      class="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
+      className="flex justify-start items-start animate-fade-in host-container active:brightness-75"
       onClick={() => props.onPromptClick?.()}
     >
       <span
-        class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
+        className="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
         data-testid="host-bubble"
         style={{
           width: 'max-content',
-          'font-size': '15px',
-          'border-radius': '0.5em', // Combined both border-radius
+          fontSize: '15px',
+          borderRadius: '0.5em',
           cursor: 'pointer',
-          border: '2px solid #6D2077', // Added border style
-          padding: '0.5em', // Added padding for space inside the border
+          border: '2px solid #6D2077',
+          padding: '0.5em',
           background: 'linear-gradient(to right, #00338D, #6D2077)',
           color: 'white',
-
+          transition: 'transform 0.3s', // Smooth transition for the zoom effect
         }}
+        onMouseEnter={(e) => (e.currentTarget.style.transform = 'scale(1.1)')}
+        onMouseLeave={(e) => (e.currentTarget.style.transform = 'scale(1)')}
       >
         {props.prompt}
       </span>
