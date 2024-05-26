09bbb69e5d82b4d02754e92e9caf295f05ae7486
simar525 <ssimarpreet11@gmail.com>
2024-05-26 18:02:55 +0530

    v31

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -4,51 +4,28 @@ type Props = {
 };
 
 export const StarterPromptBubble = (props: Props) => (
-  <div
-    data-modal-target="defaultModal"
-    data-modal-toggle="defaultModal"
-    className="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
-    onClick={() => props.onPromptClick?.()}
-    style={{
-      display: 'inline-block',
-      position: 'relative',
-      background: 'white', // Background for inner content
-      borderRadius: '12px', 
-      padding: '8px', 
-      cursor: 'pointer',
-    }}
-  >
-    <span
-      className="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
-      data-testid="host-bubble"
-      style={{
-        display: 'inline-block',
-        fontSize: '15px',
-        backgroundImage: 'linear-gradient(to right, violet, red)',
-        WebkitBackgroundClip: 'text',
-        color: 'transparent',
-        padding: '6px 12px', // Adjust padding as needed
-        borderRadius: '12px',
-        border: '2px solid transparent',
-        backgroundClip: 'padding-box',
-      }}
-    >
-      {props.prompt}
-    </span>
+  <>
     <div
-      style={{
-        content: '""',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        borderRadius: '12px',
-        border: '2px solid',
-        borderImageSlice: 1,
-        borderImageSource: 'linear-gradient(to right, violet, red)',
-        pointerEvents: 'none',
-      }}
-    />
-  </div>
+      data-modal-target="defaultModal"
+      data-modal-toggle="defaultModal"
+      class="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
+      onClick={() => props.onPromptClick?.()}
+    >
+      <span
+        class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
+        data-testid="host-bubble"
+        style={{
+          width: 'max-content',
+          'font-size': '15px',
+          'border-radius': '0.5em', // Combined both border-radius
+          cursor: 'pointer',
+          border: '1px solid purple', // Added border style
+          padding: '0.5em', // Added padding for space inside the border
+          background: 'violet',
+        }}
+      >
+        {props.prompt}
+      </span>
+    </div>
+  </>
 );
