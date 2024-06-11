30c651b3a8d25392151dad892abd8064245b61c0
simar525 <ssimarpreet11@gmail.com>
2024-06-11 16:48:43 +0530

    v102

--- src/components/bubbles/KPMGLoadingAnimation.tsx
+++ src/components/bubbles/KPMGLoadingAnimation.tsx
@@ -1,5 +1,10 @@
-export const KPMGLoadingAnimation = () => (
-  <div class="flex items-center">
-    <div class="w-2 h-2 mr-1 rounded-full kpmg-logo" />
-  </div>
-);
+export const KPMGLoadingAnimation = () => {
+  return (
+    <div class="flex justify-center items-center space-x-2">
+      <div class="w-3 h-3 rounded-full bg-violet-500 animate-bounce bounce-delay-0"/>
+      <div class="w-3 h-3 rounded-full bg-red-500 animate-bounce bounce-delay-1"/>
+      <div class="w-3 h-3 rounded-full bg-blue-500 animate-bounce bounce-delay-2"/>
+      <div class="w-3 h-3 rounded-full bg-green-500 animate-bounce bounce-delay-3"/>
+    </div>
+  );
+};
